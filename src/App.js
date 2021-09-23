22ddbec38aa00d481ae23d0390ed9a3692dd2ca6
Pratyush <space4pratyush@gmail.com>
2021-09-23 11:42:22 +0530

    modified functions and updated some of them with arrow functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -27,6 +27,6 @@ function App() {
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
