b14572b662dbc236c4323385f4d037d60dedc08a
Pratyush <space4pratyush@gmail.com>
2021-09-23 12:27:28 +0530

    added event listener on button click

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,9 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("clicked me");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +13,14 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button
+        onClick={clickHandler}
+        // onClick={() => {
+        //   console.log("Clicked me");
+        // }}
+      >
+        Click me
+      </button>
     </Card>
   );
 }
